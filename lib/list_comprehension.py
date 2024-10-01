87b9e449f9e2a50bb7f9c0b093ab7d28d1a387a5
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-10-01 12:20:21 +0300

    final commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num_list[i] for i in range(len(num_list)) if num_list[i] % 2 == 0]
     pass
 
 def make_exclamation(sentence_list):
+    return [sentence_list[i] + "!" for i in range(len(sentence_list))]
     pass
